ae7eec0d3365804807c9fe6a71a4950dfe8a18f6
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-24 08:29:29 -0500

    Modificacion de archivos(paqutes, importaciones, tema)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./components/Header/header";
+import Contenido from "./components/Content/Content";
+import { useState } from "react";
 
 function App() {
+  const [tema, setTema] = useState('');
+
+  const asignatema = (data) => {
+    setTema(data);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen flex-wrap">
+    <div className={"flex flex-col min-h-screen flex-wrap "+tema} >
       <div className="cabecera">
-        <Header/>
+        <Header temaVal={asignatema}/>
       </div>
       <div className="contenido">
+        <Contenido/>
       </div>
     </div>
   );
