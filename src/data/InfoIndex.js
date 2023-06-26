cb14046ce456b7d13a3d6c4884c489b4c995fbd0
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-25 23:55:11 -0500

    Creacio y llenado del archivo de datos de Index

---
+++ src/data/InfoIndex.js
@@ -0,0 +1,28 @@
+export const InfoIndex={
+    img:"../../../img/porfile.jpg",
+    nombre:'JOSEHP',
+    titulo:'Tecnólogo en Gestión de Redes y Sistemas Teleinformáticos',
+    titulo2:'Perfil Profesional',
+    contenido2:'El Tecnólogo en Gestión de Redes y Sistema Teleinformáticos del ISER, se capacita en  forma integral,  humanística  y  técnica  en  las  áreas  de  la  programación,  las  redes  de comunicaciones y la adaptación de las nuevas tecnologías a su entorno, soportado en un modelo pedagógico que incorpora un alto componente de integralidad del currículo para:',
+    lista2:[
+        'Coordinar,  Supervisar  y  promover  el  conjunto  de  actividades  relacionadas  con  el manejo de herramientas  software,  desarrollo  de  software  e  implementación  de Redes de Comunicación',
+        'Estructurar, y evaluar  procesos tecnológicos  para  el  desarrollo  e  implementación de software.',
+        'Estructurar,  y  evaluar  procesos  tecnológicos  en  la  implementación  de  Redes  de Comunicaciones.',
+        'Integrar  interdisciplinariamente  en  la  formulación  y  evaluación  de  proyectos  de Redes y Sistemas Teleinformáticos.',
+        'Supervisar  las  operaciones  de adaptación  de  nuevas  tecnologías  en  entornos organizacionales.',
+        'Supervisar  y  coordinar  las  actividades  de  los  trabajadores  encargados  de  las diversas operaciones en el campo de las redes y sistemas.',
+        'Organiza establecimientos que proveen servicios técnicos en el campo de las redes de comunicaciones, Sistema Teleinformáticos y Sistemas de Información.'
+    ],
+    titulo3:'Perfil Ocupacional',
+    contenido3:'La  orientación  y  el  desarrollo  del  plan  de estudios,   le   proporcionarán   al Tecnólogo en Gestión de Redes y Sistemas Teleinformáticos  los  conocimientos  teórico-prácticos, además  de  las  habilidades  y destrezas necesarias  que  se  requieren  para  laborar  en  todas las  entidades  privadas  u oficiales, pudiendo realizar las siguientes actividades:',
+    lista3:[
+        'Ejecutor de metodologías de desarrollo tecnológico para el correcto desempeño laboral en empresas del sector de las tecnologías.',
+        'Desarrollador de soluciones informáticas.',
+        'Dirigir y controlar operaciones en las organizaciones.',
+        'Diseñador  de  soluciones   de   acuerdo   a   los  procesos   de   la   ingeniería  del software.',
+        'Gestor de infraestructuras hardware y software en las organizaciones.',
+        'Conocedor  de  las  normas  relacionadas  con  la  implementación  de  redes  de comunicación en organizaciones.',
+        'Constructor de arquitecturas tecnológicas  para desarrollos de soluciones en el área informática y de las redes de comunicaciones.',
+        'Integrador de procesos de   i+D+&   en   función   de   los   desarrollos organizacionales.'
+    ],
+}
