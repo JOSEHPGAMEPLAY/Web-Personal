40280069f0c391e668513d0bb7fe933385767da0
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-26 17:14:33 -0500

    Creacion del componente about

--- src/components/Content/about/About.jsx
+++ src/components/Content/about/About.jsx
@@ -1,7 +1,37 @@
+import { InfoAbout } from '../../../data/InfoAbout';
+import './About.css';
 
 function About() {
   return (
-    <div>About
+    <div className="about entrada">
+      <div className="imgAboutC">
+        <img src={InfoAbout.Img} className="imgAbout" alt="" />
+      </div>
+      <div className="contenidoAbout">
+        <h1 className='titulo'>
+          ACERCA DE MI
+        </h1>
+        <p className='texto'>
+          <b>Nombre: </b>
+          {InfoAbout.Nombre}
+        </p>
+        <p className='texto'>
+          <b>Origen: </b>
+          {InfoAbout.Origen}
+        </p>
+        <p className='texto'>
+          <b>Correo: </b>
+          {InfoAbout.Correo}
+        </p>
+        <p className='texto'>
+          <b>Descripcion: </b>
+          {InfoAbout.Descripcion2}
+          <br />
+          {InfoAbout.Descripcion1}
+          <br/>
+          {InfoAbout.Descripcion3}
+        </p>
+      </div>     
     </div>
   )
 }
