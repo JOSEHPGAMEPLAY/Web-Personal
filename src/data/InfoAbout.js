51557a5973a7962b1925cd8840a6546a7fa5f762
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-26 17:10:00 -0500

    Creacion y llenado del archivo de datos de about

---
+++ src/data/InfoAbout.js
@@ -0,0 +1,9 @@
+export const InfoAbout = {
+    Img:"../../img/foto.jpg",
+    Nombre:"Jose Hernando Vera Jaimes",
+    Origen:"Pamplona - Norte de Santander - Colombia",
+    Correo:"jose.kj11@hotmail.com",
+    Descripcion1:"Como tecnólogo en redes y sistemas teleinformáticos, me apasiona la programación y he adquirido experiencia trabajando con diversas tecnologías.",
+    Descripcion2:"Soy una persona apasionada por el aprendizaje constante y siempre estoy buscando nuevas tecnologías y enfoques para mejorar mi trabajo. Me entusiasma explorar y aplicar nuevas soluciones tecnológicas en mi campo para resolver problemas de manera más eficiente y efectiva.",
+    Descripcion3: "Además de mis habilidades técnicas, valoro la colaboración en equipo, la comunicación efectiva y la capacidad de adaptación a nuevos desafíos. Estoy abierto a nuevas oportunidades que me permitan crecer profesionalmente y seguir contribuyendo al desarrollo y la innovación en el campo de las redes y los sistemas teleinformáticos."
+}
