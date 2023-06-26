dfe8f5c591c84d88408b4d4b260216792d7b9ee5
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-25 23:55:42 -0500

    Creacion de la vista Index

--- src/components/index/Index.jsx
+++ src/components/index/Index.jsx
@@ -1,7 +1,51 @@
+import { InfoIndex } from '../../data/InfoIndex';
+import './Index.css';
+
 export default function Index() {
   return (
-    <div className='min-w-max min-h-full'>
-      
+    <div className="indexC entrada" >
+      <div className='imgCI'>
+        <img src={InfoIndex.img} className="imagenI"></img>
+      </div>
+      <div className="tituloCI">
+        <h1 className='tituloP text-[45px]'>
+          {InfoIndex.nombre}
+        </h1>
+        <h1 className='tituloP'>
+          {InfoIndex.titulo}
+        </h1>
+      </div>
+      <div className="infoI">
+        
+        <h2 className='tituloS'>
+          {InfoIndex.titulo2}
+        </h2>
+        <p className='texto'>
+          {InfoIndex.contenido2}
+        </p>        
+        <br />
+        <ul className='lista'>
+          {InfoIndex.lista2.map((lista)=>
+               <li key={lista} className='listaI'>{lista}</li>
+            )
+          }
+        </ul>
+        <br />
+        <h2 className='tituloS'>
+          {InfoIndex.titulo3}
+        </h2>
+        <p className='texto'>
+          {InfoIndex.contenido3}
+        </p>
+        <br />
+        <ul className='lista'>  
+        {InfoIndex.lista3.map((lista)=>
+               <li key={lista} className='listaI'>{lista}</li>
+            )
+          }
+        </ul>
+        <br />
+      </div>
     </div>
   )
 }
