0c6834aa8ab1196ba576699ce849f94c14357a80
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-27 21:39:52 -0500

    Datos del componente Estudios

---
+++ src/data/InfoEstudios.js
@@ -0,0 +1,12 @@
+export const InfoEstudios ={
+    Estudios:[
+        ["../../img/estudios/Sena.png","Tecnología en Análisis Y Desarrollo De Software.","Servicio Nacional de Aprendizaje (SENA)","2023 - Actualidad"],
+        ["../../img/estudios/Iser.png","Tecnología en Gestión de Redes y Sistemas Teleinformáticos.","INSTITUTO SUPERIOR DE EDUCACIÓN RURAL - ISER","2021 - Actualidad"],
+        ["../../img/estudios/Inesa.png","Actualización Auxiliar en Sistemas, Mantenimiento y Reparación de Computadores","Instituto Nacional en Servicio y Aprendizaje","2019 - 2021"]
+    ],
+    Cursos:[
+        ["../../img/estudios/scrum.png","Scrum Foundation Professional Certificate - SFPC™","CertiProf® Professional Knowledge","2022"],
+        ["../../img/estudios/Egg.png","Programación desde cero","Egg","2022"],
+        ["../../img/estudios/Sena.png","Metodologia De La Programacion De Sistemas Informaticos","Servicio Nacional de Aprendizaje (SENA)","2021"]
+    ]
+}
