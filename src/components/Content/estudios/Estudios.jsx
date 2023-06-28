12e2fd88ce662c3cbf880657ff6ca2de4220027f
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-27 21:40:18 -0500

    Creacion del componente Estudios

--- src/components/Content/estudios/Estudios.jsx
+++ src/components/Content/estudios/Estudios.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { InfoEstudios } from '../../../data/InfoEstudios';
+import './Estudios.css';
 
 export default function Estudios() {
   return (
-    <div>Estudios</div>
+    <div className="estudios entrada">
+      <h1 className='titulo'>Estudios</h1>
+            <div className='cursos'>
+        {InfoEstudios.Estudios.map((respuesta)=>{
+          return(
+            <div className='curso' key={respuesta.key}>
+              <img src={respuesta[0]} className="imagen" alt={respuesta[2]} />
+              <div className='datosCurso'>
+                <p>
+                  <b>{respuesta[1]}</b>
+                  <br />
+                    {respuesta[2]}
+                  <br />
+                    {respuesta[3]}
+                </p>
+              </div>
+           </div>
+          )
+        }
+        )}
+           </div>
+      <h1 className='titulo'>Cursos</h1>
+            <div className='cursos' >
+      {InfoEstudios.Cursos.map((respuesta)=>{
+          return(
+            <div className='curso' key={respuesta.key}>
+              <img src={respuesta[0]} className="imagen" alt={respuesta[2]} />
+              <div className='datosCurso'>
+                <p>
+                  <b>{respuesta[1]}</b>
+                  <br />
+                    {respuesta[2]}
+                  <br />
+                    {respuesta[3]}
+                </p>
+              </div>
+           </div>
+          )
+        }
+        )}
+           </div>
+    </div>
   )
 }
