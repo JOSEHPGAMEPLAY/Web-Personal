70e7678fa6fbb09e8b2944fdfc4f0aecd0db0ca6
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-29 18:01:02 -0500

    Creacion del archivo de datos del componente footer

---
+++ src/data/InfoFooter.js
@@ -0,0 +1,10 @@
+export const InfoFooter = {
+    simbolo:["../../img/footer/logo.png","JOSEHPGAMEPLAY"],
+    redes:[
+        ["../../img/footer/linkedin.png","Linkedin","https://www.linkedin.com/in/jose-hernando-vera-jaimes-277148250/"],
+        ["../../img/footer/gitHub.png","GitHub","https://github.com/JOSEHPGAMEPLAY"],
+        ["../../img/footer/faceboock.png","Facebook","https://www.facebook.com/josehernando.verajaimes.9"],
+        ["../../img/footer/Instagram.png","Instagram","https://www.instagram.com/josehernandoverajaimes/"],
+        ["../../img/footer/whatsaap.png","Whatsapp","https://wa.link/dud0zx"],
+    ]
+}
