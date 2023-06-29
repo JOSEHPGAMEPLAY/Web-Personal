2f398829cb84e2c1a90712cee1ed1a1ae020c1b7
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-29 01:35:21 -0500

    Agegando los datos del componente Experiencia

---
+++ src/data/InfoExperiencia.js
@@ -0,0 +1,17 @@
+export const experiencia ={
+    experiencia:[
+        ["../../img/experiencia/SinExperiencia.png"]
+    ],
+    proyectos:[
+        [
+            "Proyecto Venta de Medicamentos Farmacia",
+            "Un proyecto en el cual se desarollo un sistema de ventas hecho totalmente con java, utilizando arrays como sistema de almacenamiento.",
+            "Java."
+        ],
+        [
+            "Proyecto Venta de Caballos",
+            "Una API REST  realizada en el framework de angular como cliente y next js como servidor utilizando tanto firebase para los servicios y guardar datos, como mongo para guardar informacion.",
+            "Angular, Html, Css, Js,  Firebase, MongoDB.",
+        ]
+    ]
+}
