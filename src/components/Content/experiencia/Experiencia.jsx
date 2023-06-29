8565a3ed656b7a9559db590b9280392898202d7b
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-29 01:35:47 -0500

    Agregando el componente Experiencia

--- src/components/Content/experiencia/Experiencia.jsx
+++ src/components/Content/experiencia/Experiencia.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { experiencia } from '../../../data/InfoExperiencia'
+import './Experiencia.css'
 
 export default function Experiencia() {
   return (
-    <div>Experiencia</div>
+    <div className='experiencia entrada'>
+      <h1>Proyectos</h1>
+        {experiencia.proyectos.map(valor=>{
+          return(
+            <div key={valor} className='proyectos'>
+              <h2>{valor[0]}</h2>
+              <p>{valor[1]}</p>
+              <Link to="/skills">
+                <p className='Plink'>{valor[2]}</p>
+              </Link>
+            </div>
+          )
+        })}
+        <h1>Experiencia</h1>
+        <img src={experiencia.experiencia[0]} alt="" />
+    </div>
   )
 }
