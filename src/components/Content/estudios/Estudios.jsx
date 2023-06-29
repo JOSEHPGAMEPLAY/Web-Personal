cc783c096339b500a87e1c9ee4e6f6fd3b10d248
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-06-29 01:33:33 -0500

    Agregando enlaces a los iconos del componente estudios

--- src/components/Content/estudios/Estudios.jsx
+++ src/components/Content/estudios/Estudios.jsx
@@ -7,10 +7,14 @@ export default function Estudios() {
       <h1 className='titulo'>Estudios</h1>
             <div className='cursos'>
         {InfoEstudios.Estudios.map((respuesta)=>{
+          const redirectToPage = () => {
+            window.open(respuesta[4]);
+          };
           return(
-            <div className='curso' key={respuesta.key}>
+            <div className='curso ' key={respuesta.key}>
               <img src={respuesta[0]} 
-              className="imagen animate-jump-low animate-infinite animate-duration-[10000ms]" alt={respuesta[2]} />
+              className="imagen animate-jump-low animate-infinite animate-duration-[10000ms] cursor-pointer" 
+              alt={respuesta[2]} onClickCapture={redirectToPage} />
               <div className='datosCurso'>
                 <p>
                   <b>{respuesta[1]}</b>
@@ -28,10 +32,14 @@ export default function Estudios() {
       <h1 className='titulo'>Cursos</h1>
             <div className='cursos' >
       {InfoEstudios.Cursos.map((respuesta)=>{
+        const redirectToPage = () => {
+            window.open(respuesta[4]);
+          };
           return(
             <div className='curso' key={respuesta.key}>
               <img src={respuesta[0]} 
-              className="imagen animate-jump-low animate-infinite animate-duration-[10000ms]" alt={respuesta[2]} />
+              className="imagen animate-jump-low animate-infinite animate-duration-[10000ms] cursor-pointer" 
+              alt={respuesta[2]} onClickCapture={redirectToPage}/>
               <div className='datosCurso'>
                 <p>
                   <b>{respuesta[1]}</b>
--- src/data/InfoEstudios.js
+++ src/data/InfoEstudios.js
@@ -1,12 +1,12 @@
 export const InfoEstudios ={
     Estudios:[
-        ["../../img/estudios/Sena.png","Tecnología en Análisis Y Desarrollo De Software.","Servicio Nacional de Aprendizaje (SENA)","2023 - Actualidad"],
-        ["../../img/estudios/Iser.png","Tecnología en Gestión de Redes y Sistemas Teleinformáticos.","INSTITUTO SUPERIOR DE EDUCACIÓN RURAL - ISER","2021 - Actualidad"],
-        ["../../img/estudios/Inesa.png","Actualización Auxiliar en Sistemas, Mantenimiento y Reparación de Computadores","Instituto Nacional en Servicio y Aprendizaje","2019 - 2021"]
+        ["../../img/estudios/Sena.png","Tecnología en Análisis Y Desarrollo De Software.","Servicio Nacional de Aprendizaje (SENA)","2023 - Actualidad","https://www.sena.edu.co/"],
+        ["../../img/estudios/Iser.png","Tecnología en Gestión de Redes y Sistemas Teleinformáticos.","INSTITUTO SUPERIOR DE EDUCACIÓN RURAL - ISER","2021 - Actualidad","http://www.iser.edu.co/"],
+        ["../../img/estudios/Inesa.png","Actualización Auxiliar en Sistemas, Mantenimiento y Reparación de Computadores","Instituto Nacional en Servicio y Aprendizaje","2019 - 2021","https://institutoinesa.com/"],
     ],
     Cursos:[
-        ["../../img/estudios/scrum.png","Scrum Foundation Professional Certificate - SFPC™","CertiProf® Professional Knowledge","2022"],
-        ["../../img/estudios/Egg.png","Programación desde cero","Egg","2022"],
-        ["../../img/estudios/Sena.png","Metodologia De La Programacion De Sistemas Informaticos","Servicio Nacional de Aprendizaje (SENA)","2021"]
+        ["../../img/estudios/scrum.png","Scrum Foundation Professional Certificate - SFPC™","CertiProf® Professional Knowledge","2022","https://certiprof.com/"],
+        ["../../img/estudios/Egg.png","Programación desde cero","Egg","2022","https://egg.live/"],
+        ["../../img/estudios/Sena.png","Metodologia De La Programacion De Sistemas Informaticos","Servicio Nacional de Aprendizaje (SENA)","2021","https://www.sena.edu.co/"]
     ]
 }
